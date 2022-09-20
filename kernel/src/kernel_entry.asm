644a3e5c26d55e5ad36bbc0063e664aa0408c109
theverygaming <clemibunge@gmail.com>
2022-09-20 22:44:45 +0200

    linker script and .gitignore

--- kernel/src/kernel_entry.asm
+++ kernel/src/kernel_entry.asm
@@ -1,4 +1,5 @@
 [bits 32]
 [extern main] ; Define calling point. Must have same name as kernel.c 'main' function
+section .entry
 call main ; Calls the C function. The linker will know where it is placed in memory
 jmp $
