595df00fb5bae35fd023f9e21ca71e1e6544eb31
Tracer <43095317+TracerDS@users.noreply.github.com>
2024-11-20 20:38:29 +0100

    Updated global c/dtors

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -9,19 +9,18 @@
 
 __CPP_START__
 
-/*
 using __GlobalConstructorInit_t = void (*)();
-extern __GlobalConstructorInit_t __init_constructors_array_start[];
-extern __GlobalConstructorInit_t __init_constructors_array_end[];
+extern __GlobalConstructorInit_t __init_array_start[];
+extern __GlobalConstructorInit_t __init_array_end[];
+
 void __InitGlobalConstructors() {
-    for (auto fn = __init_constructors_array_start; fn != __init_constructors_array_end; ++fn) {
-		terminal_write(" [Global Constructors] - Calling ");
-		terminal_write(reinterpret_cast<uint64_t>(*fn));
-		terminal_write("\r\n");
+    for (auto fn = __init_array_start; fn != __init_array_end; ++fn) {
+		Terminal::terminal_write(" [Global Constructors] - Calling ");
+		Terminal::terminal_write(reinterpret_cast<uint64_t>(*fn));
+		Terminal::terminal_write("\r\n");
         (*fn)();
     }
 }
-*/
 
 class A {
 public:
@@ -39,15 +38,15 @@ void kernel_main()
 {
 	Terminal::terminal_initialize();
 	{
-		A b;
+		//A b;
 	}
-	//__InitGlobalConstructors();
+	__InitGlobalConstructors();
 	
 
 	const GDT::GDT_Descriptor* descriptor = GDT::GDT_GetDescriptor();
 	GDT::GDT_Entry* entries = (GDT::GDT_Entry*)descriptor->entry;
 
-	if(!descriptor->entry) {
+	if (!descriptor->entry) {
 		Terminal::terminal_write("GDT Entries are null\r\n");
 		return;
 	}
